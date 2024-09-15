f10be730d3378ed14be671bec506372354cba15c
positiveoutlier <chantalworp@gmail.com>
2024-09-15 17:01:35 +0200

    update 15092024

--- Snake/worpies snake.py
+++ Snake/worpies snake.py
@@ -112,7 +112,7 @@ def gameLoop():
                 elif event.key == pygame.K_DOWN:
                     y1_change = snake_movement
                     x1_change = 0
-        print(event.type, x1_change, y1_change)
+        #print(event.type, x1_change, y1_change)
 
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
             game_close = True
